5a0e3c48847e508ca144394adfcd5882f0a83bde
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-09-05 11:00:35 +0300

    Add operators +, -, +=, -=

--- 1/Matrix/include/Matrix/Matrix.h
+++ 1/Matrix/include/Matrix/Matrix.h
@@ -14,13 +14,16 @@ public:
     Matrix(T value);
     Matrix(T* begin, T* end);
     Matrix(std::initializer_list<std::initializer_list<T>> list);
-    Matrix& operator=(const Matrix& other) = default;
-    Matrix& operator=(std::initializer_list<std::initializer_list<T>> list);
     size_t get_col() const;
     size_t get_row() const;
     size_t get_size() const;
     T& at(size_t i, size_t j);
     const T& at(size_t i, size_t j) const;
+    Matrix& operator=(const Matrix& other) = default;
+    //Matrix& operator=(std::initializer_list<std::initializer_list<T>> list);
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator-=(const Matrix& other);
+    
 
 public:
     using iterator = T**;
@@ -33,5 +36,11 @@ public:
     iterator crbegin() const;
     iterator crend() const;
 };
+
+template <Numeric T, size_t col_size_, size_t row_size_>
+Matrix<T, col_size_, row_size_> operator+(const Matrix<T, col_size_, row_size_>& a, const Matrix<T, col_size_, row_size_>& b);
+
+template <Numeric T, size_t col_size_, size_t row_size_>
+Matrix<T, col_size_, row_size_> operator-(const Matrix<T, col_size_, row_size_>& a, const Matrix<T, col_size_, row_size_>& b);
 
 #endif
--- 1/Matrix/source/Matrix.cpp
+++ 1/Matrix/source/Matrix.cpp
@@ -44,7 +44,34 @@ const T& Matrix<T, col_size_, row_size_>::at(size_t i, size_t j) const{
 }
 
 /*----------------OPERATORS----------------*/
-template <Numeric T, size_t col_size_, size_t row_size_>
+/*template <Numeric T, size_t col_size_, size_t row_size_>
 Matrix<T, col_size_, row_size_>& Matrix<T, col_size_, row_size_>::operator=(std::initializer_list<std::initializer_list<T>> list){
     std::copy(list.begin(), list.end(), matrix_);
+}*/
+
+template <Numeric T, size_t col_size_, size_t row_size_>
+Matrix<T, col_size_, row_size_>& Matrix<T, col_size_, row_size_>::operator+=(const Matrix& other){
+    std::transform((*this).begin(), (*this).end(), other.begin(), (*this).begin(), std::plus<T>());
+    return *this;
 }
+
+template <Numeric T, size_t col_size_, size_t row_size_>
+Matrix<T, col_size_, row_size_>& Matrix<T, col_size_, row_size_>::operator-=(const Matrix& other){
+    std::transform((*this).begin(), (*this).end(), other.begin(), (*this).begin(), std::minus<T>());
+    return *this;
+}
+
+template <Numeric T, size_t col_size_, size_t row_size_>
+Matrix<T, col_size_, row_size_> operator+(const Matrix<T, col_size_, row_size_>& a, const Matrix<T, col_size_, row_size_>& b){
+    Matrix result = a;
+    result += b;
+    return result;
+}
+
+template <Numeric T, size_t col_size_, size_t row_size_>
+Matrix<T, col_size_, row_size_> operator-(const Matrix<T, col_size_, row_size_>& a, const Matrix<T, col_size_, row_size_>& b){
+    Matrix result = a;
+    result -= b;
+    return result;
+}
+
