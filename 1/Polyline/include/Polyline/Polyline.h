41c11f86e7ed705fd719ef2e83b11234a3d658aa
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-09-10 22:50:29 +0300

    Add ctors and equel polyline

---
+++ 1/Polyline/include/Polyline/Polyline.h
@@ -0,0 +1,64 @@
+#ifndef POLYLINE_H
+#define POLYLINE_H
+
+#include <concepts>
+#include <array>
+
+template <typename T>
+concept Numeric = std::is_arithmetic_v<T>;
+
+template <Numeric T>
+struct Point{
+    T x = 0;
+    T y = 0;
+    T z = 0;
+};
+
+template <Numeric T>
+class Polyline{
+private:
+    Point<T>* dots_ = nullptr;
+    size_t capacity_ = 0;
+    size_t size_ = 0;
+
+public:
+    Polyline(const Polyline& other) : dots_(new Point<T>[other.capacity_]), capacity_(other.capacity_), size_(other.size_){
+        std::copy(other.dots_, other.dots_ + size_, dots_);
+    }
+    Polyline(Polyline&& other) : dots_(new Point<T>[other.capacity_]), capacity_(other.capacity_), size_(other.size_){
+        std::copy(other.dots_, other.dots_ + size_, dots_);
+        other.dots_ = nullptr;
+    }
+    Polyline& operator=(Polyline other);
+    Polyline& operator=(Polyline&& other);
+    void swap(Polyline& other);
+    ~Polyline();
+    
+
+};
+
+template <Numeric T>
+Polyline<T>& Polyline<T>::operator=(Polyline<T> other){
+    swap(other);
+    return *this;
+}
+
+template <Numeric T>
+Polyline<T> &Polyline<T>::operator=(Polyline<T> &&other){
+    swap(other);
+    return *this;
+}
+
+template <Numeric T>
+void Polyline<T>::swap(Polyline<T> &other){
+    std::swap(dots_, other.dots_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(size_, other.size_);
+}
+
+template <Numeric T>
+Polyline<T>::~Polyline(){
+    delete [] dots_;
+}
+
+#endif
