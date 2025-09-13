58a888f58d7e608bdb33055bd64aad9111ddb4cf
chesnokov-an <chesnokov-an-2017@mail.ru>
2025-09-14 01:20:11 +0300

    Fixed Draw_line and project structure

--- 1/Buffer/include/Buffer/Buffer.h
+++ 1/Buffer/include/Buffer/Buffer.h
@@ -10,8 +10,8 @@
 #include <stdexcept>
 
 struct BufferPoint{
-    size_t x = 0;
-    size_t y = 0;
+    double x = 0;
+    double y = 0;
 };
 
 template <size_t height_, size_t width_>
@@ -49,28 +49,17 @@ public:
 template <size_t height_, size_t width_>
 template <Numeric T>
 BufferPoint Buffer<height_, width_>::get_point_2d(const Point<T>& point){
-    long long new_y = std::round(static_cast<long long>(point.y) - static_cast<long long>(point.x)) + static_cast<long long>(width_) / 2;
-    long long new_x = std::round((static_cast<long long>(point.x) + static_cast<long long>(point.y)) / std::sqrt(15) - static_cast<long long>(point.z) * 0.6) + static_cast<long long>(height_) * 2 / 3;
-    if(new_x < 0 || new_y < 0){
-        throw std::out_of_range("Point is not printable");
-    }
-    BufferPoint result = {static_cast<size_t>(new_x), static_cast<size_t>(new_y)};
-    if(result.x >= height_ || result.y >= width_){
-        throw std::out_of_range("Point is not printable");
-    }
+    BufferPoint result = {
+        std::round((point.x + point.y) / std::sqrt(15) - point.z * 0.6) + height_ * 2 / 3,
+        static_cast<double>(point.y) - static_cast<double>(point.x) + static_cast<double>(width_) / 2
+    };
     return result;
 }
 
 template<size_t height_, size_t width_>
 double Buffer<height_, width_>::distance_to_the_line(const BufferPoint& point, const BufferPoint& start_line, const BufferPoint& end_line){
-    double x0 = static_cast<double>(point.x);
-    double y0 = static_cast<double>(point.y);
-    double x1 = static_cast<double>(start_line.x);
-    double y1 = static_cast<double>(start_line.y);
-    double x2 = static_cast<double>(end_line.x);
-    double y2 = static_cast<double>(end_line.y);
-    double numerator = std::abs((x2 - x1)*(y1 - y0) - (x1 - x0)*(y2 - y1));
-    double denominator = std::sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    double numerator = std::abs((end_line.x - start_line.x)*(start_line.y - point.y) - (start_line.x - point.x)*(end_line.y - start_line.y));
+    double denominator = std::sqrt((end_line.x - start_line.x)*(end_line.x - start_line.x) + (end_line.y - start_line.y)*(end_line.y - start_line.y));
     return numerator / denominator;
 }
 
@@ -79,12 +68,16 @@ template <Numeric T>
 void Buffer<height_, width_>::draw_line(const Point<T>& point1, const Point<T>& point2){
     BufferPoint point_2d_1 = get_point_2d(point1);
     BufferPoint point_2d_2 = get_point_2d(point2);
-    buffer_[point_2d_1.x, point_2d_1.y] = '*';
-    buffer_[point_2d_2.x, point_2d_2.y] = '*';
-    size_t min_x = std::min(point_2d_1.x, point_2d_2.x);
-    size_t min_y = std::min(point_2d_1.y, point_2d_2.y);
-    size_t max_x = std::max(point_2d_1.x, point_2d_2.x);
-    size_t max_y = std::max(point_2d_1.y, point_2d_2.y);
+    if(point_2d_1.x < height_ && point_2d_1.x >= 0 && point_2d_1.y < width_ && point_2d_1.y >= 0){
+        buffer_[point_2d_1.x, point_2d_1.y] = '*';
+    }
+    if(point_2d_2.x < height_ && point_2d_2.x >= 0 && point_2d_2.y < width_ && point_2d_2.y >= 0){
+        buffer_[point_2d_2.x, point_2d_2.y] = '*';
+    }
+    size_t min_x = std::min(std::max(std::min(point_2d_1.x, point_2d_2.x), static_cast<double>(0)), static_cast<double>(height_-1));
+    size_t min_y = std::min(std::max(std::min(point_2d_1.y, point_2d_2.y), static_cast<double>(0)), static_cast<double>(width_-1));
+    size_t max_x = std::max(std::min(std::max(point_2d_1.x, point_2d_2.x), static_cast<double>(height_-1)), static_cast<double>(0));
+    size_t max_y = std::max(std::min(std::max(point_2d_1.y, point_2d_2.y), static_cast<double>(width_-1)), static_cast<double>(0));
     if(min_x == max_x){
         for(size_t y = min_y+1; y < max_y; y++){
             buffer_[min_x, y] = '-';
@@ -99,7 +92,7 @@ void Buffer<height_, width_>::draw_line(const Point<T>& point1, const Point<T>& 
     }
     for(size_t x = min_x+1; x < max_x; x++){
         for(size_t y = min_y+1; y < max_y; y++){
-            BufferPoint current = {x, y};
+            BufferPoint current = {static_cast<double>(x), static_cast<double>(y)};
             if(distance_to_the_line(current, point_2d_1, point_2d_2) < std::sqrt(3)/4){
                 buffer_[x, y] = '-';
             }
@@ -110,9 +103,9 @@ void Buffer<height_, width_>::draw_line(const Point<T>& point1, const Point<T>& 
 template<size_t height_, size_t width_>
 void Buffer<height_, width_>::draw_axes(){
     Point<int> O = {0, 0, 0};
-    Point<int> X = {height_, 0, 0};
-    Point<int> Y = {0, height_, 0};
-    Point<int> Z = {0, 0, height_};
+    Point<int> X = {height_-1, 0, 0};
+    Point<int> Y = {0, height_-1, 0};
+    Point<int> Z = {0, 0, height_-1};
     draw_line(O, X);
     draw_line(O, Y);
     draw_line(O, Z);
